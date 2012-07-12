9a64a36c1a2d9a39a065dadd125b54cd430cc469
s1mptom <zikomar@gmail.com>
2012-07-12 15:36:19 +0400

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cart::Application.config.secret_token = '1c2b61a2fed55706c9936ce861c781be4c4ffad5edb9c7e670bd0457b66d980b394b81bb26e6da5d8cd9641fde9a88bd6157442ffd369467dd0a2f2f441b5103'
